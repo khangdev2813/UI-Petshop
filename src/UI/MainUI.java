7e6330cd050a24ff10fdedc3b3f6a9357f50ded6
Khang <khangdev2813@gmail.com>
2023-05-25 20:46:31 +0700

    first commit

---
+++ src/UI/MainUI.java
@@ -0,0 +1,25 @@
+package UI;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Khang
+ */
+public class MainUI extends JFrame {
+    
+    public MainUI () {
+
+    }
+    public static void main(String args[]) {
+         new Login();
+     }
+}
+
